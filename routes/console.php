d6175d21115c49c92854f0dc5fcd01d4fdc48347
tjanuki <t.januki@gmail.com>
2025-05-07 16:01:40 -0400

    Add scheduled task to update all status metrics every five minutes

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Console\Commands\UpdateAllStatusMetrics;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(UpdateAllStatusMetrics::class)
+    ->everyFiveMinutes()
+    ->name('status:update-all')
+    ->withoutOverlapping()
+    ->onFailure(function () {
+        // Handle failure (e.g., send notification)
+    });
